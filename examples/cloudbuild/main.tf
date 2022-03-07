c0c74e4819438072eafbe5e51ccf94245d923f67
tomarv2 <varuntomar2019@gmail.com>
2022-03-07 10:36:30 -0800

    .

--- examples/cloudbuild/main.tf
+++ examples/cloudbuild/main.tf
@@ -1,9 +1,24 @@
+terraform {
+  required_version = ">= 1.0.1"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.12.0"
+    }
+  }
+}
+
+provider "google" {
+  region  = var.region
+  project = var.project
+}
+
+
 module "cloud_build" {
   source = "../../"
 
   owner_name  = "tomarv2"
   repo_name   = "terraform-google-cloud-build"
-  gcp_project = "demo-1000"
   #-----------------------------------------------
   # Note: Do not change teamid and prjid once set.
   teamid = var.teamid
